91fbcc72c0cb926f40eb5757a3f99fa393269574
Julian Alimin <dmastag@yahoo.com>
2019-08-10 21:28:55 +0700

    implement logic from stack overflow
    https://stackoverflow.com/questions/29846199/uploading-document-in-nodejs-using-fs-module-error-write-after-end

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-var app = require("express")();
-var multiparty = require("multiparty");
+const app = require("express")();
+const multiparty = require("multiparty");
+const fs = require('fs')
 
-app.post("/submit", function(httpRequest, httpResponse, next){
+app.post("/submit", function(req, res, next){
 
     var form = new multiparty.Form();
 
-    form.on("part", function(part){
-        if(part.filename)
-        {
-            var FormData = require("form-data");
-            var request = require("request")
-            var form = new FormData();
+    var document = {};
+    var fstream = null;
 
-            form.append("thumbnail", part, {filename: part.filename,contentType: part["content-type"]});
+    // populate fields
+    form.on("field", function (name, value) {
+        document[name] = value ;
+    });
 
-            var r = request.post("http://localhost:7070/store", { "headers": {"transfer-encoding": "chunked"} }, function(err, res, body){ 
-                httpResponse.send(res);
-            });
-            
-            r._form = form
+    form.on("part", function (part) {
+
+        if (!part.filename) {
+            return;
         }
-    })
+
+        var path = part.filename;
+
+        fstream = fs.createWriteStream(path);
+
+        document.type = part.headers["content-type"]
+        document.name = part.filename;
+        document.size = part.byteCount;
+        document.path = path;
+
+        fstream.on("close", function () {
+            res.send(document)
+            res.end();
+        });
+        part.pipe(fstream);
+
+    });
+
+    form.on("close", function (data) {
+
+        fstream.end();
+        fstream = null;
+
+    });
+
+    form.parse(req);
 
     form.on("error", function(error){
         console.log(error);
     })
-
-    form.parse(httpRequest);    
     
 });
 
 app.get("/", function(httpRequest, httpResponse, next){ 
-    httpResponse.send("<form action='http://localhost:9090/submit' method='post' enctype='multipart/form-data'><input type='file' name='thumbnail' /><input type='submit' value='Submit' /></form>");
+    httpResponse.send("<form action='http://localhost:8080/submit' method='post' enctype='multipart/form-data'><input type='file' name='thumbnail' /><input type='submit' value='Submit' /></form>");
 });
 
 app.listen(8080);
